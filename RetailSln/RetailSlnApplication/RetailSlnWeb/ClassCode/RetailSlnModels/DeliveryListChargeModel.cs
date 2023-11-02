33cc93d17d36cced76480ccc12e42640ea0b2433
shangeethR <shangeethravi2000@gmail.com>
2023-11-02 22:53:23 +0530

    RetailSln

---
+++ RetailSln/RetailSlnApplication/RetailSlnWeb/ClassCode/RetailSlnModels/DeliveryListChargeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetailSlnModels
+{
+    public class DeliveryListChargeModel
+    {
+        public long DeliveryListChargeId { set; get; }
+
+        public long ClientId { set; get; }
+
+        public long DeliveryListId { set; get; }
+
+        public string ChargeTypeDesc {  set; get; }
+
+        public string ChargeTypeNameDesc { set; get; }
+
+        public string ChargeValueType {  set; get; }
+
+        public float ChargeValue { set; get; }
+    }
+}
---
+++ RetailSln/RetailSlnApplication/RetailSlnWeb/ClassCode/RetailSlnModels/DeliveryListModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetailSlnModels
+{
+    public class DeliveryListModel
+    {
+        public List<DeliveryListChargeModel> DeliveryListChargeModel { set; get; }
+
+        public List<DeliveryChargeModel> DeliveryChargeModel { set; get; }
+
+        public long DeliveryListId { set; get; }
+
+        public long ClientId { set; get; }
+
+        public string DeliveryListIName { set; get; }
+        
+    }
+}
