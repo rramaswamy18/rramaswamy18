faf1cfe48c5a314d3beefc5a9d3ce51338399000
rramaswamy18 <50352555+rramaswamy18@users.noreply.github.com>
2023-12-12 08:47:10 -0800

    TemplePrd

---
+++ TemplePrd/TemplePrdApplication/TemplePrdWeb/App_Start/Startup.Auth.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TemplePrdWeb
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                ExpireTimeSpan = TimeSpan.FromMinutes(int.Parse(ConfigurationManager.AppSettings["AccessTokenExpiryMinutes"])), //Get it from Config
+                LoginPath = new PathString("/Home/Unauthorized"),
+                SlidingExpiration = true,
+            });
+        }
+    }
+}
