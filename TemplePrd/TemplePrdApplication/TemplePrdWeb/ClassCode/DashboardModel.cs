c9d6a27bf90d149c75ba92d988b8c3659eb0070d
rramaswamy18 <50352555+rramaswamy18@users.noreply.github.com>
2023-12-20 08:29:12 -0800

    TemplePrd

---
+++ TemplePrd/TemplePrdApplication/TemplePrdWeb/ClassCode/DashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using ArchitectureLibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetailAdmModels
+{
+    public class DashboardModel
+    {
+        public ResponseObjectModel ResponseObjectModel { set; get; }
+    }
+}
--- TemplePrd/TemplePrdApplication/TemplePrdWeb/Controllers/HomeController.cs
+++ TemplePrd/TemplePrdApplication/TemplePrdWeb/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArchitectureLibraryCacheData;
+using ArchitectureLibraryUtility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,19 +10,39 @@ namespace TemplePrdWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private string clientSuffix;
+        private long clientId;
+        private string clientName;
+        private readonly string execUniqueId = Utilities.CreateExecUniqueId();
+        private readonly string lastIpAddress = Utilities.GetLastIPAddress();
+        public HomeController()
+        {
+        }
+
         public ActionResult Index()
         {
+            string absoluteUri = Request.Url.AbsoluteUri;
+            if (absoluteUri.ToUpper().IndexOf("FREMONT") > -1)
+            {
+                clientSuffix = "Fremont";
+            }
+            else
+            {
+                clientSuffix = "Sacramento";
+            }
+            clientId = long.Parse(Utilities.GetApplicationValue("ClientId" + clientSuffix));
+            clientName = Utilities.GetApplicationValue("ClientName" + clientSuffix);
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AboutUs()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult ContactUs()
         {
             ViewBag.Message = "Your contact page.";
 
